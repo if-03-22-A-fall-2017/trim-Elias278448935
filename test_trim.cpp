56eee500b207a8fa417a49cb677352ce4450d293
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2017-12-09 19:33:47 +0100

    Add files via upload

--- test_trim.cpp
+++ test_trim.cpp
@@ -16,6 +16,7 @@
 #include "trim.h"
 #include "test_trim.h"
 
+
 /******************************************************************************
  * empty_substring: tests that trimming an empty string yields an empty
  * string.
@@ -51,7 +52,7 @@ TEST(total_string)
  ******************************************************************************/
 TEST(leading_blanks)
 {
-	char str[STRLEN] = "    PRPR is fun";
+	char str[STRLEN] = "   PRPR is fun";
 	char trimmed[STRLEN] = "no content";
 
 	trim(str, trimmed);
--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,70 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+ #include "shortcut.h"
+ #include "trim.h"
+#include <string.h>
+
+
+
+
+ void trim(const char* str, char* trimmed)
+ {
+   int ende = 0;
+   int anfang  = 0;
+   int count = 0;
+   int boolian = 0;
+
+    for (int i = 0; i < STRLEN; i++)
+    {
+      if(str[i] != ' ' && boolian != 1)
+      {
+       anfang = i;
+       boolian = 1;
+      }
+    }
+
+    for (int i = 0 ; i < STRLEN; i++)
+    {
+      if(str[i] != ' ' && str[i] != '\0')
+      {
+        ende = i;
+      }
+    }
+
+    for (int i = 0; i < STRLEN; i++)
+    {
+      if(i >= anfang && i <= ende)
+      {
+        trimmed[count] = str[i];
+        count++;
+
+      }
+    }
+
+
+
+
+
+
+  /* int flag = 0;
+   int count = 0;
+
+    for (int i = 0; i < STRLEN; i++)
+    {
+      if(str[i] != ' ')
+      {
+        flag = 1;
+      }
+        if(flag = 1)
+        {
+          trimmed[i] = str[count];
+          count++;
+        }
+
+
+
+    }
+*/
+
+ }
